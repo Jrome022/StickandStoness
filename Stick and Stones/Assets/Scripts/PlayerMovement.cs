1650d39aef69ed827fde32a348dea211d1664100
Jrome022 <qjescalona@tip.edu.ph>
2024-11-03 15:04:16 +0800

    bg layer changes

--- Stick and Stones/Assets/Scripts/PlayerMovement.cs
+++ Stick and Stones/Assets/Scripts/PlayerMovement.cs
@@ -4,48 +4,65 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    private Rigidbody2D body;
-    private Animator anim;
-    private bool grounded;
+     public Rigidbody2D rb;
+     public Animator anim;
+     public float movement;
+     public float speed = 5f;
+     public float jumpHeight = 7f;
+     public bool isGround = true;
+     private bool facingRight = true;
+     void Start()
+     {
 
-    private void Awake()
-    {
-        //Get references for rigidbody and animator
-        body = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
-
-    }
-    private void Update()
-    {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-
-        // When moving left and right
-        if (horizontalInput > 0.01f)
-              transform.localScale = Vector3.one;
- 
-        else if (horizontalInput < -0.01f)
-              transform.localScale = new Vector3(-1, 1, 1);
-
-        if(Input.GetKey(KeyCode.Space) && grounded)
+     }
+     void Update()
+     {
+          movement = Input.GetAxis("Horizontal");
+          transform.position += new Vector3(movement * Time.deltaTime * speed, 0f, 0f);
+          Flip();
+          if (Input.GetKey(KeyCode.Space) && isGround)
+          {
               Jump();
+              anim.SetBool("Jump", true);
+              isGround = false;
+          }
 
-        //Set animator parameters
-        anim.SetBool("Run", horizontalInput != 0);
-        anim.SetBool("Grounded", grounded);
+          if (Mathf.Abs(movement) > .1f)
+          {
+              anim.SetFloat("Run", 1f);
+          }
+
+          else if (movement < .1f)
+          {
+              anim.SetFloat("Run", 0f);
+          }
+     }
+    void Jump()
+    {
+         Vector2 velocity = rb.velocity;
+         velocity.y = jumpHeight;
+         rb.velocity = velocity;
     }
 
-    private void Jump()
+    void Flip()
     {
-        body.velocity = new Vector2(body.velocity.x, speed);
-        anim.SetTrigger("Jump");
-        grounded = false;
+        if(movement < 0f && facingRight)
+        {
+            transform.eulerAngles = new Vector3(0f, -180f, 0f);
+            facingRight = false;
+        }
+        else if(movement > 0f && facingRight == false)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            facingRight = true;
+        }
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    void OnCollisionEnter2D(Collision2D other)
     {
-        if(collision.gameObject.tag == "Ground")
-           grounded = true;
+        if (other.collider.tag == "Ground")
+        {
+            isGround = true;
+            anim.SetBool("Jump", false);
+        }
     }
 }
