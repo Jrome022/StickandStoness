fb20242a7b37adf3a0898f9dc7edfeb57e76d5f0
Jrome022 <qjescalona@tip.edu.ph>
2024-11-04 20:54:53 +0800

    level 1 test

---
+++ Stick and Stones/Assets/Scripts/Follow.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform player;
+    public float smoothNess = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPos = new Vector3(player.position.x, player.position.y, player.position.z);
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothNess * Time.deltaTime);
+
+    }
+}
